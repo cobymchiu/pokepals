3ada48c0340d6f7a3483e347fdbe74babfaa88e2
unknown <johnsonchristianna9@gmail.com>
2022-04-05 12:39:23 -0400

    citations and helpers

--- index.php
+++ index.php
@@ -1,10 +1,17 @@
 <?php
-//used template files from class and reused login components from previous homeworks
 // server url: https://cs4640.cs.virginia.edu/cc6hkb/cs4640-project/
 // extra credit google cloud url: https://storage.googleapis.com/cs4640-pokepals/index.html
+
+//Sources
+//used template files from class and reused login components from previous homeworks
 //https://stackoverflow.com/questions/17122218/get-all-the-images-from-a-folder-in-php
 //https://stackoverflow.com/questions/23711508/regular-expression-to-match-at-least-one-capital-letter-and-at-least-one-digit-a
 //https://stackoverflow.com/questions/19345689/mysql-exclude-all-blocked-users-from-the-results
+
+//Helpers
+//Nour Goulmamine ()-helped arrange our setup.php to automate the database creation
+//Christian () - helped create the regex for password validation
+
 session_start();
 // Register the autoloader
 spl_autoload_register(function($classname) {
